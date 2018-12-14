afcfd0477b1e53d0f903642161811c49c75c5842
Vyacheslav <ws.sazonov@gmail.com>
2018-12-14 21:27:25 +0300

    Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatSidenavModule, MatListModule, MatSelectModule,
   MatButtonModule, MatIconModule, MatMenuModule, MatExpansionModule } from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
 import { ContentComponent } from './content/content.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainNavComponent,
-    ContentComponent
+    ContentComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
+    FlexLayoutModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     LayoutModule,
@@ -28,10 +34,12 @@ import { ContentComponent } from './content/content.component';
     MatIconModule,
     MatMenuModule,
     MatExpansionModule,
-    MatButtonModule
+    MatButtonModule,
+    NgbModule.forRoot(),
   ],
   exports: [
     BrowserModule,
+    FlexLayoutModule,
     MatToolbarModule,
     MatSidenavModule,
     MatSelectModule,
