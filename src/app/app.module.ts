41e7c01ec832b4880cb0d9487d3e8f0cadd743e7
Vyacheslav <ws.sazonov@gmail.com>
2018-12-14 18:41:19 +0300

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatSidenavModule, MatListModule, MatSelectModule,
+  MatButtonModule, MatIconModule, MatMenuModule, MatExpansionModule } from '@angular/material';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatSelectModule,
+    MatListModule,
+    MatIconModule,
+    MatMenuModule,
+    MatExpansionModule,
+    MatButtonModule
+  ],
+  exports: [
+    BrowserModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatSelectModule,
+    MatListModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatExpansionModule,
     AppRoutingModule
   ],
   providers: [],
