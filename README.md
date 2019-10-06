771f2c34599acfd9c21bfd110ee0f41db11b7a61
Viacheslav Sazonov <viacheslav.sazonov@seagullinc.co>
2019-10-06 19:08:31 +0300

    Create angular app

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
+# App
 
-# testOnAngular
-Don't forgot do `npm i`
-# MyApp
-
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.1.3.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.0.
 
 ## Development server
 
@@ -28,4 +25,3 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,53 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { MainNavComponent } from './main-nav/main-nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatSidenavModule, MatListModule, MatSelectModule,
-  MatButtonModule, MatIconModule, MatMenuModule, MatExpansionModule } from '@angular/material';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import { ContentComponent } from './content/content.component';
-import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainNavComponent,
-    ContentComponent,
-    SliderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule,
-    FlexLayoutModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatSelectModule,
-    MatListModule,
-    MatIconModule,
-    MatMenuModule,
-    MatExpansionModule,
-    MatButtonModule,
-    NgbModule.forRoot(),
-  ],
-  exports: [
-    BrowserModule,
-    FlexLayoutModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatSelectModule,
-    MatListModule,
-    MatIconModule,
-    MatMenuModule,
-    MatButtonModule,
-    MatExpansionModule,
     AppRoutingModule
   ],
   providers: [],
